3ee2e87af92a0a95d1dc9e03dbc73060c89a8b6c
YenYuHsuan <allen20046@gmail.com>
2017-07-12 23:30:58 +0800

    upload the code and readme.md

---
+++ ros-function.js
@@ -0,0 +1,48 @@
+
+
+var ROS_IP = 'xxx.xxx.xxx.xxx:9090';
+
+// Connect to ROS.
+var ros = new ROSLIB.Ros({
+  url : 'ws://'+ROS_IP
+});
+ros.on('connection', function() {
+  console.log('Connected to ros server.');
+});
+
+ros.on('error', function(error) {
+  console.log('Error connecting to ros server: ', error);
+});
+
+ros.on('close', function() {
+  console.log('Connection to ros server closed.');
+});
+
+// Publisher 
+function ros_pub(topic, msg_type, payload){
+	//define the topic
+	var pub_topic = new ROSLIB.Topic({
+		ros : ros,
+		name : topic,
+		messageType : msg_type
+	});
+	//publish to the topic
+	var pub_msg = new ROSLIB.Message({
+		payload
+	});
+	pub_topic.publish(pub_msg);
+}
+
+// Subscriber
+function ros_sub(topic, msg_type){
+	//define the topic
+	var sub_topic = new ROSLIB.Topic({
+		ros : ros,
+		name : topic,
+		messageType : msg_type
+	});
+	//subscribe from the topic
+	sub_topic.subscribe(function(message) {
+		console.log("Subscribe to topic " + topic + ", recieved message:" + message);
+	});
+}
